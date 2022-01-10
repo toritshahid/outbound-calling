7c0ba2a6b5f10a334266e9a50a97272a729e2310
Syed shahid shabir qadri <53717633+shahidshabir055@users.noreply.github.com>
2022-01-10 14:30:37 +0530

    changes

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from '../login/login.component';
+import { OtpComponent } from '../otp/otp.component';
 import { HomePage } from './home.page';
 
 const routes: Routes = [
   {
     path: '',
     component: LoginComponent,
+  },
+  {
+    path:'otp',
+    component: OtpComponent
+
   }
 ];
 
